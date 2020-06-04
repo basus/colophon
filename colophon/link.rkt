f6c564e69b1e4b7f45e2f89238c496388334e121
Shrutarshi Basu <shr@basus.me>
2020-06-03 21:10:47 -0400

    Add utility functions for generating links

---
+++ colophon/link.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(require racket/string
+         txexpr xml/path
+         pollen/core pollen/decode)
+
+(define (directory d)
+  (let* (
+         [name (string-downcase d)]
+         [href (format "/~a/" name)]
+         [here-path (select-from-metas 'here-path (current-metas))]
+         [attrs (if (member name (explode-path here-path))
+                    `((href ,href) (class "current") )
+                    `((href ,href) ))])
+    `(li (a ,attrs ,name))))
+
+(define (title name)
+  `(li (a ((href "/")) (strong ,(string-downcase name)))))
+
+(provide (all-defined-out))
